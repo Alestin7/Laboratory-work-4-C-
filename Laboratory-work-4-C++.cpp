ef22eab02f9d025f861ae4e025c5ea7ca0e3c1c3
Алексей Костин <kostinalesha837@gmail.com>
2020-06-03 21:39:21 +0300

    Добавьте файлы проекта.

---
+++ Laboratory-work-4-C++.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+void Base_lvl()
+{
+    cout << "Base task:" << endl;
+    cout << "Enter a sentence: ";
+
+    string str;
+    getline(cin, str);
+    str.pop_back();
+    cout << str << "\n\n";
+}
+
+void Medium_lvl()
+{
+    cout << "Medium task:" << endl;
+    cout << "Enter a sentence: ";
+
+    string line;
+    string new_line;
+
+    getline(cin, line);
+
+    for (int i = 0; i < (int)(line.size()); i++)
+    {
+        if (line[i] == ' ' || (int)(line[i]) > 47 && (int)(line[i]) < 58)
+        {
+            if (line[i] == ' ')
+                new_line += "  ";
+        }
+        else
+            new_line += line[i];
+    }
+
+    cout << new_line << "\n\n";
+}
+
+int main()
+{
+    Base_lvl();
+    Medium_lvl();
+}
